1a5f537d872543e620b3f4e675646d04232ec9e1
Silas <silasishallahan@gmail.com>
2019-02-25 14:12:26 -0800

    completed basic features - Silas

--- index.js
+++ index.js
@@ -2,25 +2,32 @@
 /* global cuid */
 
 
-const STORE = [
-  {id: cuid(), name: 'apples', checked: false},
-  {id: cuid(), name: 'oranges', checked: false},
-  {id: cuid(), name: 'milk', checked: true},
-  {id: cuid(), name: 'bread', checked: false}
-];
+const STORE = {
+  
+  items: [
+    {id: cuid(), name: 'apples', checked: false},
+    {id: cuid(), name: 'oranges', checked: false},
+    {id: cuid(), name: 'milk', checked: true},
+    {id: cuid(), name: 'bread', checked: false}
+  ],
+  hideCompleted: false,
+};
 
 
-function renderShoppingList() {
+function renderShoppingList(arr) {
 
   // this function will be responsible for rendering the shopping list in
   // the DOM
 
+  let filteredItems = arr;
 
-  console.log('`renderShoppingList` ran');
+  if (STORE.hideCompleted){
+    filteredItems = filteredItems.filter((item) => !item.checked);
+  }
 
+  $('.js-shopping-list').html(generateShoppingItemString(filteredItems));
+} 
 
-  $('.js-shopping-list').html(generateShoppingItemString(STORE));
-}
 
 
 function generateShoppingItemString(shoppingList){
@@ -44,15 +51,34 @@ function generateItemElement(item){
         <button class="shopping-item-delete js-item-delete">
             <span class="button-label">delete</span>
         </button>
+        <form id="js-rename-form" class="js-rename">
+            <label for="shopping-list-rename"></label>
+            <input type="text" name="shopping-list-rename" class="shopping-list-rename" placeholder="...">
+            <button type="submit" class="js-rename-button">Rename</button>
+        </form>
       </div>
     </li>`
   ;
 }
 
 
+function toggleHideFilter(){
+  STORE.hideCompleted = !STORE.hideCompleted;
+}
+
+
+function handleToggleHideFilter(){
+  $('.js-hide-completed-toggle').click(() => {
+    console.log('click');
+    toggleHideFilter();
+    renderShoppingList(STORE.items);
+  });
+}
+
+
 function addItemToShoppingList(itemName){
   console.log(`adding ${itemName} to shopping list`);
-  STORE.push({id: cuid(), name: itemName, checked: false});
+  STORE.items.push({id: cuid(), name: itemName, checked: false});
 }
 
 
@@ -71,10 +97,34 @@ function handleNewItemSubmit() {
     $('.js-shopping-list-entry').val('');
 
     addItemToShoppingList(newItemName);
-    renderShoppingList();
+    renderShoppingList(STORE.items);
   });
 }
 
+
+function handleNewItemSearch(){
+
+  $('#js-search-form').submit(function(event){
+    event.preventDefault();
+
+    const searchItem = $('.js-shopping-list-search').val();
+
+    if(searchForItem(searchItem).length > 0){
+      const resultObj = searchForItem(searchItem);
+      console.log(resultObj);
+
+      renderShoppingList(resultObj);
+    } 
+
+  });
+  
+}
+
+
+function searchForItem(item){
+  const result = STORE.items.filter((val) => val.name === item);
+  return result;
+}
 
 function getItemIdFromElement(item){
   return $(item).closest('li').data('item-id');
@@ -82,7 +132,7 @@ function getItemIdFromElement(item){
 
 
 function getIndexOfSelectedItem(item){
-  return STORE.findIndex((storeVal) => storeVal.id === item);
+  return STORE.items.findIndex((storeVal) => storeVal.id === item);
 }
 
 
@@ -98,8 +148,8 @@ function handleItemCheckClicked() {
     const itemIndex = getIndexOfSelectedItem(itemId);
     console.log(itemIndex);
 
-    STORE[itemIndex].checked = ! STORE[itemIndex].checked;
-    renderShoppingList();
+    STORE.items[itemIndex].checked = ! STORE.items[itemIndex].checked;
+    renderShoppingList(STORE.items);
     
   });
 }
@@ -115,21 +165,42 @@ function handleDeleteItemClicked() {
 
     const itemIndex = getIndexOfSelectedItem(itemId);
 
-    STORE.splice(itemIndex, 1);
-    renderShoppingList();
+    STORE.items.splice(itemIndex, 1);
+    renderShoppingList(STORE.items);
   });
 }
+
+
+function handleRenameItem(){
+  $('.js-rename').submit(function(event){
+    event.preventDefault();
+
+    const newName = $('.shopping-list-rename').val();
+    const selectedItem = getItemIdFromElement(this);
+
+    const itemIndex = getIndexOfSelectedItem(selectedItem);
+
+    STORE.items[itemIndex].name = newName;
+
+    renderShoppingList(STORE.items);
+
+    console.log(itemIndex);
+  });
+}
+
 
 // this function will be our callback when the page loads. it's responsible for
 // initially rendering the shopping list, and activating our individual functions
 // that handle new item submission and user clicks on the "check" and "delete" buttons
 // for individual shopping list items.
 function handleShoppingList() {
-  renderShoppingList();
+  renderShoppingList(STORE.items);
   handleNewItemSubmit();
+  handleNewItemSearch();
   handleItemCheckClicked();
   handleDeleteItemClicked();
-
+  handleRenameItem();
+  handleToggleHideFilter();
 }
 
 // when the page loads, call `handleShoppingList`
